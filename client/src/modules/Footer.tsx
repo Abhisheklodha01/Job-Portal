4571404a74cd86828a5d61345d0772224063cbc9
abhisheklodha001 <abhishek.lodha.jobsdoor360@gmail.com>
2024-11-11 18:54:47 +0530

    created footer

--- client/src/modules/Footer.tsx
+++ client/src/modules/Footer.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-black text-xl'>
+        <h3 className='text-center text-white pt-10'>Email us: support@jobportal.com</h3>
+      <div className='flex flex-col md:flex-row justify-evenly items-center
+       text-white pb-5 pt-4'>
+        <Link to={"/"}>LinkedIn</Link>
+        <Link to={"/"}>Twitter</Link>
+        <Link to={"/"}>Instgram</Link>
+      </div>
+      <h2 className='text-white text-xl text-center pb-5'>All rights reserved @2024</h2>
+    </div>
   )
 }
